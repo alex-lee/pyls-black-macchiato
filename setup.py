88635bec51fb1d82075ac9f231a99a1f01e6dfe2
Alex Lee <alex@collat.io>
2020-04-28 13:53:33 -0500

    Add license and setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyls-black-macchiato",
+    version="0.1",
+    description="Black-macchiato plugin for the Python Language Server",
+    url="https://github.com/alex-lee/pyls-black-macchiato",
+    author="Alex Lee",
+    author_email="alex@collat.io",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["python-language-server", "black-macchiato"],
+    extras_require={"dev": ["flake8", "mypy", "pytest"]},
+    entry_points={"pyls": ["pyls_black_macchiato = pyls_black_macchiato.plugin"]},
+)
