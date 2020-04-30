fb9de39887f68a446ad3436fbb45faf082c0aae5
Alex Lee <alex@collat.io>
2020-04-29 23:16:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="pyls-black-macchiato",
-    version="0.1",
-    description="Black-macchiato plugin for the Python Language Server",
-    url="https://github.com/alex-lee/pyls-black-macchiato",
+    version="0.1.0",
     author="Alex Lee",
     author_email="alex@collat.io",
-    packages=find_packages(exclude=["tests"]),
-    install_requires=[
-        "python-language-server",
-        (
-            "black-macchiato @ "
-            "git+https://github.com/alex-lee/black-macchiato.git@refactor-for-plugin-use"
-        ),
-        "typing-extensions",
-    ],
+    description="Black-macchiato plugin for the Python Language Server",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alex-lee/pyls-black-macchiato",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    install_requires=["black", "python-language-server", "typing-extensions"],
     extras_require={"dev": ["flake8", "mypy", "pytest"]},
     entry_points={"pyls": ["pyls_black_macchiato = pyls_black_macchiato.plugin"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
